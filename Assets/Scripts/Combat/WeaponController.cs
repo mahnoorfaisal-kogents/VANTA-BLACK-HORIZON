99c79399eaa1910c4c883e872bd8b9a42e507392
mahnoorfaisal-kogents <mahnoorfaisal.kg@gmail.com>
2026-09-24 06:41:04 -0700

    feat: add weapon spread and pellet handling

--- Assets/Scripts/Combat/WeaponController.cs
+++ Assets/Scripts/Combat/WeaponController.cs
@@ -10,36 +10,57 @@ namespace Vanta.Combat
         [SerializeField] private Camera aimCamera;
         [SerializeField] private Transform muzzle;
         public int AmmoInMagazine { get; private set; }
+        public bool IsReloading => reloading;
         private float nextShotTime;
         private bool reloading;
 
-        private void Awake() { if (weapon) AmmoInMagazine = weapon.magazineSize; }
+        private void Awake()
+        {
+            if (weapon) AmmoInMagazine = weapon.magazineSize;
+        }
+
         private void Update()
         {
             if (!weapon || reloading) return;
             if (Input.GetKeyDown(KeyCode.R)) { StartCoroutine(Reload()); return; }
             if (Input.GetButton("Fire1")) TryFire();
         }
+
         private void TryFire()
         {
             if (Time.time < nextShotTime) return;
             if (AmmoInMagazine <= 0) { StartCoroutine(Reload()); return; }
+
             nextShotTime = Time.time + 1f / Mathf.Max(0.01f, weapon.roundsPerSecond);
             AmmoInMagazine--;
+
             var cam = aimCamera ? aimCamera : Camera.main;
             if (!cam) return;
-            var ray = cam.ViewportPointToRay(new Vector3(.5f,.5f,0));
-            if (Physics.Raycast(ray, out var hit, weapon.range, weapon.hitMask, QueryTriggerInteraction.Ignore))
+
+            for (var pellet = 0; pellet < Mathf.Max(1, weapon.pellets); pellet++)
             {
+                var spread = Random.insideUnitCircle * Mathf.Tan(weapon.spreadDegrees * Mathf.Deg2Rad);
+                var direction = (cam.transform.forward +
+                                 cam.transform.right * spread.x +
+                                 cam.transform.up * spread.y).normalized;
+                var origin = muzzle ? muzzle.position : cam.transform.position;
+
+                if (!Physics.Raycast(origin, direction, out var hit, weapon.range,
+                    weapon.hitMask, QueryTriggerInteraction.Ignore))
+                    continue;
+
                 var damageable = hit.collider.GetComponentInParent<IDamageable>();
                 damageable?.ApplyDamage(weapon.damage, hit.point, gameObject);
             }
         }
+
         private IEnumerator Reload()
         {
             if (reloading || !weapon || AmmoInMagazine >= weapon.magazineSize) yield break;
-            reloading = true; yield return new WaitForSeconds(weapon.reloadSeconds);
-            AmmoInMagazine = weapon.magazineSize; reloading = false;
+            reloading = true;
+            yield return new WaitForSeconds(weapon.reloadSeconds);
+            AmmoInMagazine = weapon.magazineSize;
+            reloading = false;
         }
     }
 }
