51691eabf8a2eaa88f618937701154e91d1b75f4
mahnoorfaisal-kogents <mahnoorfaisal.kg@gmail.com>
2026-09-24 06:51:28 -0700

    feat: add loadout vehicle archetypes and pursuit tech foundations

---
+++ Assets/Scripts/Vehicles/VehicleArchetype.cs
@@ -0,0 +1,14 @@
+namespace Vanta.Vehicles
+{
+    public enum VehicleArchetype
+    {
+        Compact,
+        Sedan,
+        Muscle,
+        Sports,
+        OffRoad,
+        Motorcycle,
+        Utility,
+        Emergency
+    }
+}
