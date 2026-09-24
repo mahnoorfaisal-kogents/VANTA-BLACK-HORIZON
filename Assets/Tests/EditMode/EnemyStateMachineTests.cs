9082f7c0bdac3bd563ec3a778eb1284d060c61db
mahnoorfaisal-kogents <mahnoorfaisal.kg@gmail.com>
2026-09-24 07:27:22 -0700

    test: cover enemy state transition guards

--- Assets/Tests/EditMode/EnemyStateMachineTests.cs
+++ Assets/Tests/EditMode/EnemyStateMachineTests.cs
@@ -6,11 +6,30 @@ namespace Vanta.Tests
     public sealed class EnemyStateMachineTests
     {
         [Test] public void StartsIdle() => Assert.That(new EnemyStateMachine().Current, Is.EqualTo(EnemyState.Idle));
-        [Test] public void TransitionChangesCurrentState()
+
+        [Test]
+        public void TransitionChangesCurrentState()
         {
             var machine = new EnemyStateMachine();
-            machine.Transition(EnemyState.Combat);
+            Assert.That(machine.Transition(EnemyState.Combat), Is.True);
             Assert.That(machine.Current, Is.EqualTo(EnemyState.Combat));
+        }
+
+        [Test]
+        public void DeadEnemyCannotReturnToCombat()
+        {
+            var machine = new EnemyStateMachine();
+            Assert.That(machine.Transition(EnemyState.Dead), Is.True);
+            Assert.That(machine.Transition(EnemyState.Combat), Is.False);
+            Assert.That(machine.Current, Is.EqualTo(EnemyState.Dead));
+        }
+
+        [Test]
+        public void IdleCannotJumpDirectlyToChase()
+        {
+            var machine = new EnemyStateMachine();
+            Assert.That(machine.Transition(EnemyState.Chase), Is.False);
+            Assert.That(machine.Current, Is.EqualTo(EnemyState.Idle));
         }
     }
 }
