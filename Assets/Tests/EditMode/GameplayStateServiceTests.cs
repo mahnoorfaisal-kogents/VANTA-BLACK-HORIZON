dbd9ae7a8150ad99f60ffca770cd0f40af08ba9a
mahnoorfaisal-kogents <mahnoorfaisal.kg@gmail.com>
2026-09-24 08:57:31 -0700

    Add gameplay state reset lifecycle regression tests

--- Assets/Tests/EditMode/GameplayStateServiceTests.cs
+++ Assets/Tests/EditMode/GameplayStateServiceTests.cs
@@ -17,5 +17,43 @@ namespace Vanta.Tests
             Assert.That(service.TrySet(GameplayState.Paused), Is.False);
             Assert.That(service.TrySet(GameplayState.Playing), Is.True);
         }
+
+        [Test]
+        public void ResetRaisesChangeEventAndReturnsToMainMenu()
+        {
+            var service = new GameplayStateService();
+            var changes = 0;
+            GameplayState previous = default;
+            GameplayState next = default;
+            service.Changed += (from, to) =>
+            {
+                changes++;
+                previous = from;
+                next = to;
+            };
+
+            Assert.That(service.TrySet(GameplayState.Playing), Is.True);
+            changes = 0;
+
+            service.Reset();
+
+            Assert.That(service.Current, Is.EqualTo(GameplayState.MainMenu));
+            Assert.That(changes, Is.EqualTo(1));
+            Assert.That(previous, Is.EqualTo(GameplayState.Playing));
+            Assert.That(next, Is.EqualTo(GameplayState.MainMenu));
+        }
+
+        [Test]
+        public void ResetIsIdempotentWhenAlreadyInMainMenu()
+        {
+            var service = new GameplayStateService();
+            var changes = 0;
+            service.Changed += (_, _) => changes++;
+
+            service.Reset();
+
+            Assert.That(changes, Is.Zero);
+            Assert.That(service.Current, Is.EqualTo(GameplayState.MainMenu));
+        }
     }
 }
