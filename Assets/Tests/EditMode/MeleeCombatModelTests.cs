0951e022e3aed28c50070f0a8f141cab1020ca3b
mahnoorfaisal-kogents <mahnoorfaisal.kg@gmail.com>
2026-09-25 08:04:51 -0700

    test: define melee attack timing and damage contract

---
+++ Assets/Tests/EditMode/MeleeCombatModelTests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using Vanta.Combat;
+
+public sealed class MeleeCombatModelTests
+{
+    [Test]
+    public void AttackConsumesCooldownAndAllowsNextAttackAfterInterval()
+    {
+        var model = new MeleeCombatModel(25f, 1f);
+        Assert.IsTrue(model.TryAttack(0f));
+        Assert.IsFalse(model.TryAttack(0.5f));
+        Assert.IsTrue(model.TryAttack(1f));
+    }
+
+    [Test]
+    public void InvalidAttackParametersAreBounded()
+    {
+        var model = new MeleeCombatModel(-5f, -1f);
+        Assert.AreEqual(0f, model.Damage);
+        Assert.IsTrue(model.TryAttack(0f));
+        Assert.IsFalse(model.TryAttack(0f));
+    }
+}
