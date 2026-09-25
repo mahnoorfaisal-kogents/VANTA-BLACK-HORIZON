36496fae8b82c4d5053779897b699217ecb736d1
mahnoorfaisal-kogents <mahnoorfaisal.kg@gmail.com>
2026-09-25 08:01:50 -0700

    test: define deterministic combat health behavior

---
+++ Assets/Tests/EditMode/CombatHealthModelTests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using Vanta.Combat;
+
+public sealed class CombatHealthModelTests
+{
+    [Test]
+    public void DamageReducesHealthAndDestroysAtZero()
+    {
+        var model = new CombatHealthModel(100f);
+        Assert.IsTrue(model.ApplyDamage(35f));
+        Assert.AreEqual(65f, model.Health, 0.001f);
+        Assert.IsFalse(model.IsDead);
+
+        Assert.IsTrue(model.ApplyDamage(65f));
+        Assert.AreEqual(0f, model.Health, 0.001f);
+        Assert.IsTrue(model.IsDead);
+    }
+
+    [Test]
+    public void DeadTargetCannotTakeAdditionalDamage()
+    {
+        var model = new CombatHealthModel(25f);
+        Assert.IsTrue(model.ApplyDamage(25f));
+        Assert.IsFalse(model.ApplyDamage(10f));
+        Assert.AreEqual(0f, model.Health, 0.001f);
+    }
+
+    [Test]
+    public void DamageAndHealthAreBounded()
+    {
+        var model = new CombatHealthModel(100f);
+        Assert.IsFalse(model.ApplyDamage(0f));
+        Assert.IsFalse(model.ApplyDamage(-10f));
+        Assert.AreEqual(100f, model.Health, 0.001f);
+    }
+}
