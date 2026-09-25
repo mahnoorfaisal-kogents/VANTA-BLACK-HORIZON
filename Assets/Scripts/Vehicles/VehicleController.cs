4048cf05f363af919e59f7fd1c4b0b80a2d36f04
mahnoorfaisal-kogents <mahnoorfaisal.kg@gmail.com>
2026-09-25 07:46:53 -0700

    feat: integrate garage upgrades into vehicle handling

--- Assets/Scripts/Vehicles/VehicleController.cs
+++ Assets/Scripts/Vehicles/VehicleController.cs
@@ -12,10 +12,17 @@ namespace Vanta.Vehicles
         [SerializeField] float maxHealth = 100f;
         [SerializeField] float baseGrip = 1f;
         [SerializeField, Range(0f, 1f)] float minimumGrip = 0.35f;
+        [Header("Garage")]
+        [SerializeField, Range(0f, 1f)] float engineUpgrade;
+        [SerializeField, Range(0f, 1f)] float chassisUpgrade;
+        [SerializeField, Range(0f, 1f)] float tireUpgrade;
+        [SerializeField, Range(0f, 1f)] float drivetrainUpgrade;
+        [SerializeField, Range(0f, 1f)] float auxiliaryUpgrade;
 
         Rigidbody body;
         VehicleDamageState damageState;
         VehicleHandlingModel handlingModel;
+        VehicleGarageModel garageModel;
 
         public float Health => damageState?.Health ?? 0f;
         public bool IsDestroyed => damageState?.IsDestroyed ?? true;
@@ -26,6 +33,12 @@ namespace Vanta.Vehicles
             body = GetComponent<Rigidbody>();
             damageState = new VehicleDamageState(maxHealth);
             handlingModel = new VehicleHandlingModel(maxSpeed, baseGrip, minimumGrip);
+            garageModel = new VehicleGarageModel(maxHealth, baseGrip, maxSpeed);
+            garageModel.Install(VehiclePartType.Engine, engineUpgrade);
+            garageModel.Install(VehiclePartType.Chassis, chassisUpgrade);
+            garageModel.Install(VehiclePartType.Tires, tireUpgrade);
+            garageModel.Install(VehiclePartType.Drivetrain, drivetrainUpgrade);
+            garageModel.Install(VehiclePartType.Auxiliary, auxiliaryUpgrade);
             damageState.Destroyed += HandleDestroyed;
         }
 
@@ -41,9 +54,11 @@ namespace Vanta.Vehicles
             float throttle = Input.GetAxis("Vertical");
             float steer = Input.GetAxis("Horizontal");
             var healthPercent = maxHealth <= 0f ? 0f : Health / maxHealth * 100f;
-            var effectiveSpeed = handlingModel.EffectiveMaxSpeed(healthPercent);
-            var effectiveGrip = handlingModel.EffectiveGrip(healthPercent);
-            body.AddForce(transform.forward * throttle * acceleration, ForceMode.Acceleration);
+            var effectiveSpeed = Mathf.Min(handlingModel.EffectiveMaxSpeed(healthPercent), garageModel.EffectiveMaxSpeed);
+            var effectiveGrip = Mathf.Max(handlingModel.EffectiveGrip(healthPercent), minimumGrip * 0.5f);
+            var effectiveAcceleration = garageModel.EffectiveAcceleration * (acceleration / 12f);
+
+            body.AddForce(transform.forward * throttle * effectiveAcceleration, ForceMode.Acceleration);
             body.MoveRotation(body.rotation * Quaternion.Euler(
                 0,
                 steer * turnRate * effectiveGrip * Time.fixedDeltaTime * Mathf.Clamp01(body.velocity.magnitude / 5f),
@@ -53,10 +68,24 @@ namespace Vanta.Vehicles
                 body.velocity = body.velocity.normalized * effectiveSpeed;
         }
 
-        public void ApplyDamage(float amount)
+        public bool InstallPart(VehiclePartType type, float normalizedUpgrade)
         {
-            damageState?.ApplyDamage(amount);
+            if (garageModel == null || !garageModel.Install(type, normalizedUpgrade))
+                return false;
+
+            switch (type)
+            {
+                case VehiclePartType.Engine: engineUpgrade = normalizedUpgrade; break;
+                case VehiclePartType.Chassis: chassisUpgrade = normalizedUpgrade; break;
+                case VehiclePartType.Tires: tireUpgrade = normalizedUpgrade; break;
+                case VehiclePartType.Drivetrain: drivetrainUpgrade = normalizedUpgrade; break;
+                case VehiclePartType.Auxiliary: auxiliaryUpgrade = normalizedUpgrade; break;
+            }
+
+            return true;
         }
+
+        public void ApplyDamage(float amount) => damageState?.ApplyDamage(amount);
 
         public void Repair(float amount)
         {
